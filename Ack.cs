72beaa088608f5ebf4552856123876c1e5541df0
Niko Jovan <q@qq>
2021-04-06 17:20:49 -0400

    write Ack function

---
+++ Ack.cs
@@ -0,0 +1,23 @@
+﻿namespace Ackermann
+{
+    public static class Ack
+    {
+        public static int A(int m, int n)
+        {
+            if (m == 0)
+            {
+                return n + 1;
+            }
+            else if (n == 0)
+            {
+                return A(m - 1, 1);
+            }
+            return A(m - 1, A(m, n - 1));
+        }
+
+        public static bool CheckArgs(int m, int n)
+        {
+            return m >= 0 && n >= 0;
+        }
+    }
+}
